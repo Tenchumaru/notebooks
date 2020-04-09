ba94cc7e4f89be59f6eb55e7792b1178262188b5
Chris Idzerda <cidzerda@twitch.tv>
2020-04-08 17:01:01 -0700

    Convert prime_factors into a generator function

--- utilities.py
+++ utilities.py
@@ -109,13 +109,11 @@ def demonstrate_partition():
 
 def prime_factors(n):
     i = 2
-    factors = []
     while i * i <= n:
         if n % i:
             i += 1
         else:
             n //= i
-            factors.append(i)
+            yield i
     if n > 1:
-        factors.append(n)
-    return factors
+        yield n
