c315a6eb2f89a8e5bb4e882aebd3456b011a60bf
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2025-11-08 12:10:11 -0800

    Update requirements.txt

--- CreateVenv.cmd
+++ CreateVenv.cmd
@@ -1,14 +1,12 @@
 @ECHO OFF
 
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" -m venv --system-site-packages venv
+CALL %SystemDrive%\local\bin\CreateVenv.cmd
 IF ERRORLEVEL 1 GOTO error
 CALL venv\Scripts\activate.bat
 IF ERRORLEVEL 1 GOTO error
 python -m pip install --upgrade pip
 IF ERRORLEVEL 1 GOTO error
 pip install -U setuptools
-IF ERRORLEVEL 1 GOTO error
-pip install torch===1.6.0 torchvision===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 IF ERRORLEVEL 1 GOTO error
 pip install -r requirements.txt
 IF ERRORLEVEL 1 GOTO error
